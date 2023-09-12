c35c7a6e06944ba0cc3642dbd85196114abf1920
Scarrus666 <scarrus@gmail.com>
2023-09-12 16:42:03 +0200

    test commit

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,23 @@ export default
           title: "Welcome to Opportunity",
           sheet_id: import.meta.env.VITE_GOOGLE_SHEET_ID,
           api_token: import.meta.env.VITE_GOOGLE_API_KEY,
+
+
         };
       },
+
+      methods: 
+      {
+        async getData()  
+          {
+            const response = await fetch(this.gsheet_url);
+            const data = await response.json();
+            this.entries = data.valueRanges[0].values;
+          }
+    }
   };
+
+
 </script>
 
 <template>
@@ -27,8 +41,57 @@ export default
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #323d4a;
+  background-color: lightcyan;
   margin: 60px;
   font-size: 62px;
+}
+.card
+{
+  background-color: darkblue;
+  padding: 10px;
+  margin-top: 15px;
+}
+.site-date
+{
+  color: grey;
+}
+.timeStamp
+{
+  color: tomato;
+  font-weight: bold;
+  font-size: large;
+}
+.cardTitle
+{
+  color: lightsalmon;
+  font-size: large;
+  font-weight: bold;
+}
+.cardBeschreibung
+{
+  color: lightcoral;
+}
+
+#footerLogos
+{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 10px;
+  color: darkblue;
+}
+.logosSponsors
+{
+  width: 200px;
+  height: 50px;
+}
+
+footer 
+{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: white;
 }
 
 </style>
